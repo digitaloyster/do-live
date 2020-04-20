145e0221174269e7ffc369abd54c86abbd8f749e
Tai Goodwin <t41g00dw1n@gmail.com>
2020-04-20 11:09:23 +0100

    Just clearing staging

---
+++ ms-linted/msh.js
@@ -0,0 +1,29 @@
+/**
+ * Multistep Script V1.3
+ */
+
+document.cdnMultiStep.hooks = {
+  hooks: [],
+  register: function( name, callback ) {
+    if ( 'undefined' == typeof( document.cdnMultiStep.hooks[name] ) ) {
+      document.cdnMultiStep.hooks[name] = [];
+    }
+    document.cdnMultiStep.hooks[name].push( callback );
+  },
+
+  call: function( name, args ) {
+    if ( 'undefined' != typeof( document.cdnMultiStep.hooks[name] ) ) {
+      let returns;
+      for ( i = 0; i < document.cdnMultiStep.hooks[name].length; ++i ) {
+        const data = document.cdnMultiStep.hooks[name][i]( args );
+        if ( data ) {
+          if ( returns === undefined ) {
+            returns = [];
+          }
+          returns.push(data);
+        }
+      }
+      return returns;
+    }
+  },
+};
