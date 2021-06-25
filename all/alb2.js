aaf02a87da74dc1f2bbd842d18c3988ba910384d
Timothy Coxon <timothy@digitaloyster.uk>
2021-06-25 11:43:48 +0100

    Removed gPBN declaration

--- all/alb2.js
+++ all/alb2.js
@@ -1,15 +1,15 @@
 // All Pages Scripts Before Body CDN-v2
 
 // Functions
-const getParameterByName = function(name) {
-  const url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-  const results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-};
+//const getParameterByName = function(name) {
+//  const url = window.location.href;
+//  name = name.replace(/[\[\]]/g, '\\$&');
+//  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+//  const results = regex.exec(url);
+//  if (!results) return null;
+//  if (!results[2]) return '';
+//  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+//};
 
 // Fix Styling Issues
 if (typeof window.ub !== 'undefined') window.ub.page.disableTextAdjustments = true;
