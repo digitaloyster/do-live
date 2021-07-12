2509c8800067d3401624f996eec580f04aa8ce40
Timothy Coxon <timothy@digitaloyster.uk>
2021-07-12 10:38:32 +0100

    Expand scope on msb variables

--- ms-2/msb.js
+++ ms-2/msb.js
@@ -2,28 +2,31 @@
 /* jslint long */
 // Version 1.2.1
 
-$(document).ready(function () {
+$(document).ready(function() {
+  let steps = {};
+  let settings = {};
+  let hooks = {};
   // XXX: Variables/Objects
   const d = false;
   if (document.cdnMultiStep.debugMode) d = true;
 
   if (document.cdnMultiStep.steps != '') {
-    const steps = document.cdnMultiStep.steps;
+    steps = document.cdnMultiStep.steps;
   } else alert('steps not found');
   // var buttons = document.cdnMultiStep.buttons;
   if (document.cdnMultiStep.settings != '') {
-    const settings = document.cdnMultiStep.settings;
+    settings = document.cdnMultiStep.settings;
   } else alert('settings not found');
 
   if (document.cdnMultiStep.hooks != '') {
-    const hooks = document.cdnMultiStep.hooks;
+    hooks = document.cdnMultiStep.hooks;
   } else alert('hooks not found');
 
   // Variables/Objects
   /* ------------------------------------------------------------------------ */
   // XXX: Functions
   // Initialise Step Structre
-  const initialise = function () {
+  const initialise = function() {
     hooks.call('hookPreInit', []); // Hook
     let msBrowser = false;
     let oldIOS = false;
@@ -43,28 +46,28 @@ $(document).ready(function () {
 
     $('#' + document.cdnMultiStep.settings.nextButton).addClass('button-next');
     $('#' + document.cdnMultiStep.settings.nextButton).addClass('button-back');
-    $('.lp-pom-button').each(function () {
+    $('.lp-pom-button').each(function() {
       let mousedown = 0;
-      $('#' + this.id).mousedown(function (e) {
+      $('#' + this.id).mousedown(function(e) {
         mousedown = 1;
       });
-      $('#' + this.id).focus(function (e) {
+      $('#' + this.id).focus(function(e) {
         if (!mousedown) {
           $(this).addClass('focusGlow');
         }
         mousedown = 0;
       });
-      $('#' + this.id).blur(function () {
+      $('#' + this.id).blur(function() {
         $(this).removeClass('focusGlow');
       });
-      $(this).keypress(function (ev) {
+      $(this).keypress(function(ev) {
         ev.preventDefault();
         focusClick(ev, this, 1);
       });
     });
-    $.each(steps, function (i, val) {
+    $.each(steps, function(i, val) {
       const page = [];
-      $.each(steps[i].fields, function (k, val) {
+      $.each(steps[i].fields, function(k, val) {
         page.push('#container_' + k);
       });
       if (objSize(page) == 0) {
@@ -75,9 +78,9 @@ $(document).ready(function () {
     });
 
     // Add custom aspects
-    $.each(steps, function (i, val) {
+    $.each(steps, function(i, val) {
       if ('fields' in steps[i] && steps[i].fields != '') {
-        $.each(steps[i].fields, function (k, val) {
+        $.each(steps[i].fields, function(k, val) {
           // Email Validate
           // if (k==="email_address") {
           //    document.getElementById(k).setAttribute(
@@ -93,7 +96,7 @@ $(document).ready(function () {
               if (objSize(steps[i].fields) === 1 && $('[name="' + k + '"]')[0].type !== 'checkbox') {
                 $('[name="' + k + '"]').parent().addClass('single-field');
               }
-              $('[name="' + k + '"]').bind('change', function () {
+              $('[name="' + k + '"]').bind('change', function() {
                 const $update = $(this).parent('div');
                 if ($('[name="' + k + '"]')[0].type !== 'checkbox') {
                   $('#container_' + k + ' .selected').removeClass('selected');
@@ -136,7 +139,7 @@ $(document).ready(function () {
             const field = document.getElementById(k);
             field.setAttribute('pattern', '[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])');
           }
-          if ("mask" in val) {
+          if ('mask' in val) {
             $('#' + k).mask(val.mask);
           }
         });
@@ -154,17 +157,17 @@ $(document).ready(function () {
     hooks.call('hookPageInit', []); // HOOK
   };
 
-  const loadStyleSheet = function (thisURL) {
+  const loadStyleSheet = function(thisURL) {
     const styles = document.createElement('link');
     styles.setAttribute('href', thisURL);
     styles.setAttribute('rel', 'stylesheet');
     styles.setAttribute('type', 'text/css');
     document.head.appendChild(styles);
-  }
+  };
 
-  const loadScript = function (thisURL, option) {
+  const loadScript = function(thisURL, option) {
     const script = document.createElement('script');
-    script.onload = function () {
+    script.onload = function() {
       for (const el in option) {
         if ({}.hasOwnProperty.call(option, el)) {
           $('#' + el).datepicker(option[el]);
@@ -173,22 +176,22 @@ $(document).ready(function () {
     };
     script.src = thisURL;
     document.head.appendChild(script);
-  }
+  };
 
   // D8 Validation
-  const d8Validate = function () {
+  const d8Validate = function() {
     if (d) console.log('d8Validate()');
     // TODO: Switch custom validity from hardcoded to set variable if exists.
     document.getElementById('telephone').setCustomValidity('Please enter a valid telephone number.');
     const event = new Event('d8Validate');
     document.dispatchEvent(event);
-  }
+  };
 
   // Validate current page
-  const isValid = function (step) {
+  const isValid = function(step) {
     const valid = true;
     const event = new Event('doErrors');
-    $.each(steps[step].fields, function (i, v) {
+    $.each(steps[step].fields, function(i, v) {
       i = i.trim();
       if ('postcode' in document.cdnParameters && document.cdnParameters.postcode != 'N') {
         if ((i == 'add1' || i == 'postcode') && $('#' + i).val() != '') {
@@ -217,17 +220,17 @@ $(document).ready(function () {
   };
 
   // Clear all error messages
-  const clearErrors = function () {
+  const clearErrors = function() {
     $('.error-message').remove();
   };
 
   // Get all Elements
-  const getElements = function () {
+  const getElements = function() {
     const allElements = [];
-    $.each(steps, function (index, value) {
+    $.each(steps, function(index, value) {
       if ('elements' in steps[index] && steps[index].elements != '') {
         const elements = steps[index].elements.split(',');
-        $.each(elements, function (ind, val) {
+        $.each(elements, function(ind, val) {
           allElements.push('#' + val.trim());
         });
       }
@@ -236,18 +239,18 @@ $(document).ready(function () {
   };
 
   // Show current pages elements
-  const showElements = function (step) {
+  const showElements = function(step) {
     $(getElements()).fadeOut(400);
     if ('elements' in steps[step] && steps[step].elements != '') {
       const elements = steps[step].elements.split(',');
-      $.each(elements, function (index, value) {
+      $.each(elements, function(index, value) {
         $('#' + value.trim()).fadeIn(1000);
       });
     }
   };
 
   // Goto specific step
-  const gotoStep = function (step) {
+  const gotoStep = function(step) {
     // TODO: Sort out fadein for fields on step change
     $('.active').removeClass('active');
     $('#step-' + step).addClass('active');
@@ -282,36 +285,36 @@ $(document).ready(function () {
     hooks.call('hookNewStep', []); // HOOK
   };
 
-  const focusClick = function (ev, el) {
+  const focusClick = function(ev, el) {
     if (ev.keyCode === 32 || ev.keyCode === 13) {
       $(':input:enabled:visible:first').focus();
       el.click();
     }
   };
 
-  const updateTabIndex = function () {
-    $('button').each(function () {
-      $('#' + this.id).focus(function () {
+  const updateTabIndex = function() {
+    $('button').each(function() {
+      $('#' + this.id).focus(function() {
         $(this).addClass('focusGlow');
       });
-      $('#' + this.id).blur(function () {
+      $('#' + this.id).blur(function() {
         $(this).removeClass('focusGlow');
       });
-      $(this).keypress(function (ev) {
+      $(this).keypress(function(ev) {
         ev.preventDefault();
         focusClick(ev, this);
       });
     });
     let count = 1;
     // $( "#step-" + step + ' *' ).filter(':input').each(function( ) {
-    $('form *').filter(':input').each(function () {
+    $('form *').filter(':input').each(function() {
       $(this).attr('tabindex', count);
       count++;
     });
     $('#' + document.cdnMultiStep.settings.nextButton).attr('tabindex', count);
     count++;
     $('#' + document.cdnMultiStep.settings.prevButton).attr('tabindex', count);
-    $('.lp-pom-button').each(function () {
+    $('.lp-pom-button').each(function() {
       if (this.id != document.cdnMultiStep.settings.nextButton && this.id != document.cdnMultiStep.settings.prevButton) {
         $(this).attr('tabindex', count);
         count++;
@@ -320,13 +323,13 @@ $(document).ready(function () {
   };
 
   // Fade between steps
-  const showStep = function () {
+  const showStep = function() {
     $('fieldset > div').fadeOut(400);
     $('.active').fadeIn(1000);
   };
 
   // Detect and return current step
-  const getStep = function () {
+  const getStep = function() {
     return $('.active').attr('data-id');
   };
 
@@ -334,12 +337,12 @@ $(document).ready(function () {
   // Utility Functions
 
   // Return size of object
-  const objSize = function (obj) {
+  const objSize = function(obj) {
     return Object.keys(obj).length;
   };
 
   // Scroll to top of form
-  const refocusForm = function () {
+  const refocusForm = function() {
     const step = getStep();
     if (step != 1) {
       if ('refocus' in document.cdnParameters && document.cdnParameters.refocus != 'N') {
@@ -357,7 +360,7 @@ $(document).ready(function () {
   // XXX: Click Handlers
 
   // Goto prev step
-  const prevStep = function () {
+  const prevStep = function() {
     if ('hookPrevCheck' in hooks && !hooks.call('hookPrevCheck', [])) return; // HOOK
     const step = getStep();
     gotoStep(--step);
@@ -365,7 +368,7 @@ $(document).ready(function () {
   };
 
   // Goto next step
-  const nextStep = function () {
+  const nextStep = function() {
     const step = getStep();
     if (isValid(step)) {
       if ('hookNextCheck' in hooks && !hooks.call('hookNextCheck', [])) return; // HOOK
@@ -377,7 +380,7 @@ $(document).ready(function () {
   };
 
   // StepPixel
-  const stepPixel = function (page) {
+  const stepPixel = function(page) {
     if (document.cdnParameters.cake_offer_id != '' && typeof document.cdnParameters.cake_offer_id !== 'undefined' && steps[page].event != '' && typeof steps[page].event !== 'undefined' && document.cdnParameters.lp_tracking_prefix != '' && typeof document.cdnParameters.lp_tracking_prefix !== 'undefined') {
       // REVIEW: Added check for cake_offer_id AND cake_lp_event_id AND lp_tracking_prefix
       const image = new Image(1, 1);
@@ -389,7 +392,7 @@ $(document).ready(function () {
   };
 
   // Submit functions
-  const submit = function () {
+  const submit = function() {
     if (isValid(getStep())) {
       if (d) console.log('Submitted');
       else {
@@ -404,8 +407,8 @@ $(document).ready(function () {
     }
   };
 
-  const submitActive = function () {
-    $('#' + settings.submitButton).click(function (e) {
+  const submitActive = function() {
+    $('#' + settings.submitButton).click(function(e) {
       console.log('submitbutton specific');
       e.preventDefault();
       if ('data8' in document.cdnParameters && document.cdnParameters.data8 == 'Y') d8Validate();
@@ -420,35 +423,35 @@ $(document).ready(function () {
   $('#' + settings.nextButton).click(nextStep);
   $('#' + settings.prevButton).click(prevStep);
   submitActive();
-  $('.select-button.single-field label').click(function () {
+  $('.select-button.single-field label').click(function() {
     console.log('single button click');
     nextStep();
   });
-  document.addEventListener('submitActive', function () {
+  document.addEventListener('submitActive', function() {
     submitActive();
   });
 
-  document.addEventListener('d8Complete', function () {
+  document.addEventListener('d8Complete', function() {
     if (isValid(getStep())) {
       submit();
     }
   });
 
   // Custom Event listeners for page control.
-  document.addEventListener('nextStep', function () {
+  document.addEventListener('nextStep', function() {
     nextStep();
     console.log('nextStep heard');
   });
-  document.addEventListener('prevStep', function () {
+  document.addEventListener('prevStep', function() {
     prevStep();
   });
-  document.addEventListener('gotoStep', function (e) {
+  document.addEventListener('gotoStep', function(e) {
     console.log(e.detail);
     gotoStep(e.detail);
   });
 
   // Prevent enter key functionality
-  $(window).keydown(function (event) {
+  $(window).keydown(function(event) {
     if (event.keyCode == 13 || event.keyCode == 169) {
       event.preventDefault();
       return false;
