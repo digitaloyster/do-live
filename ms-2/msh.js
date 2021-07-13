a83c74b544a7c34b84da3cf35dc38aa65092a798
Timothy Coxon <timothy@digitaloyster.uk>
2021-07-13 11:18:35 +0100

    Renamed Arguments to Arg in MSH

--- ms-2/msh.js
+++ ms-2/msh.js
@@ -1,26 +1,26 @@
 // Version 1.1
 document.cdnMultiStep.hooks = {
   hooks: [],
-  register: function ( name, callback ) {
-    if( 'undefined' == typeof( document.cdnMultiStep.hooks[name] ) )
-      document.cdnMultiStep.hooks[name] = []
-      document.cdnMultiStep.hooks[name].push( callback )
-    },
-
-    call: function ( name, arguments ) {
-      if( 'undefined' != typeof( document.cdnMultiStep.hooks[name] ) ) {
-        var returns;
-        for( i = 0; i < document.cdnMultiStep.hooks[name].length; ++i ) {
-          var data = document.cdnMultiStep.hooks[name][i]( arguments );
-          if( data ){
-            if( returns === undefined ){
-              returns = [];
-            }
-            returns.push(data);
-          }
-
-        }
-        return returns;
-      }
+  register: function( name, callback ) {
+    if ( 'undefined' == typeof( document.cdnMultiStep.hooks[name] ) ) {
+      document.cdnMultiStep.hooks[name] = [];
     }
+    document.cdnMultiStep.hooks[name].push( callback );
+  },
+
+  call: function( name, arg ) {
+    if ( 'undefined' != typeof( document.cdnMultiStep.hooks[name] ) ) {
+      let returns;
+      for ( i = 0; i < document.cdnMultiStep.hooks[name].length; ++i ) {
+        const data = document.cdnMultiStep.hooks[name][i]( arg );
+        if ( data ) {
+          if ( returns === undefined ) {
+            returns = [];
+          }
+          returns.push(data);
+        }
+      }
+      return returns;
+    }
+  },
 };
