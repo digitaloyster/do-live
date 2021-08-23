43663c84d66835de6f3bdcdc462fa80633302576
Timothy Coxon <timothy@digitaloyster.uk>
2021-08-23 09:52:11 +0100

    Removed a duplicate function from tyb

--- all-2/tyb.js
+++ all-2/tyb.js
@@ -1,21 +1,21 @@
 // All Thank You Pages Before Body CDN-v2.0
 //
 // Functions
-const getCookie = function(cname) {
-  const name = cname + '=';
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  for (let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return '';
-};
+// const getCookie = function(cname) {
+//  const name = cname + '=';
+//  const decodedCookie = decodeURIComponent(document.cookie);
+//  const ca = decodedCookie.split(';');
+//  for (let i = 0; i <ca.length; i++) {
+//    let c = ca[i];
+//    while (c.charAt(0) == ' ') {
+//      c = c.substring(1);
+//    }
+//    if (c.indexOf(name) == 0) {
+//      return c.substring(name.length, c.length);
+//    }
+//  }
+//  return '';
+// };
 // Functions
 //
 $(document).ready(function() {
