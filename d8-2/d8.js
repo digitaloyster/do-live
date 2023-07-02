2ee80e11e2ebcae9274d37a2324344953f198eab
Timothy Coxon <Timothy.Coxon@gmail.com>
2023-07-02 12:31:39 +0100

    Updates to D8 to include older settings

--- d8-2/d8.js
+++ d8-2/d8.js
@@ -11,6 +11,7 @@ const d8Validation = {
     useLineValidation: true,
     useMobileValidation: true,
     defaultCountryCode: 44,
+    allowedPrefixes: “+441,+442,+447”,
     msg: 'Please enter a valid telephone number.',
   },
 };
